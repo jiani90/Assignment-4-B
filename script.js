f6b15c7a1e6cc7c5fb4ac9ba1e05d420e5b01e2f
Jia Ni Zheng <jiani90@Jias-MacBook-Pro.local>
2015-11-05 15:46:28 -0500

    confused on var timeSeries

--- script.js
+++ script.js
@@ -36,24 +36,111 @@ plot.append('g').attr('class','axis axis-y')
     .call(axisY);
 
 //Start importing data
-d3.csv('/data/world_bank_2012.csv', parse, dataLoaded);
+//queue()
+//    .defer(d3.csv('/data/fao_combined_world_1963_2013.csv',parse)
+//    .await(dataLoaded))
+
+var lineGenerator = d3.svg.line()
+    .x(function(d){ return scaleX(d.year)})
+    .y(function(d){ return scaleY(d.value)});
+
+
+d3.csv('/data/fao_combined_world_1963_2013.csv',parse,dataLoaded);
 
 function parse(d){
-
-    //Eliminate records for which gdp per capita isn't available
-
-    //Check "primary completion" and "urban population" columns
-    //if figure is unavailable and denoted as "..", replace it with undefined
-    //otherwise, parse the figure into numbers
     return {
+        item: d.ItemName,
+        value: +d.Value,
+        year: +d.Year
 
-    };
+    }
 
 
 
 }
 
-function dataLoaded(error, rows){
+function dataLoaded(error, data) {
+    var nestedData = d3.nest()
+        .key(function (d) {
+            return d.item
+        })
+        .entries(data);
+
+    plot.append('path')
+        .attr('class', 'data-line coffee-data-line')
+        .datum(nestedData[0].values)
+        .attr('d', lineGenerator);
+    plot.append('path')
+        .attr('class', 'data-line tea-data-line')
+        .datum(nestedData[1].values)
+        .attr('d', lineGenerator)
+        //.call(attachTooltip)
+    //plot
+    //  .append('circle')
+    //  .attr('class', 'data-line coffee-data-point')
+    // .datum('nestedData'.values)
+
+    plot.selectAll('t')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('class','t')
+        .attr('cx',function(d){return scaleX(d.year)})
+        .attr('cy',function(d){return scaleY(d.value)})
+        .attr('r',2)
+        .style('fill-opacity',.1)
+        .call(attachTooltip);
+
+
 
 }
+//var dots = plot.selectAll('.data-line')
+//    .data(data);
+//var dotsEnter = dots.enter()
+//    .append('line')
+//    .attr('class','data-line')
+//
+//
+//dots.exit().transition().remove();
+//dots
+//    .transition()
+//    .attr('cx',function(d){return scaleX(d.year)})
+//    .attr('cy',function(d){return scaleY(d.value)});
 
+function attachTooltip(selection){
+    selection
+        .on('mouseenter',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            tooltip
+                .transition()
+                .style('opacity',1);
+
+            tooltip.select('#item').html(d.item);
+            tooltip.select('#year').html(d.year);
+            tooltip.select('#value').html(d.value);
+        })
+        .on('mousemove',function(){
+            var xy = d3.mouse(canvas.node());
+            console.log(xy);
+
+            var tooltip = d3.select('.custom-tooltip');
+
+            tooltip
+                .style('left',xy[0]+50+'px')
+                .style('top',(xy[1]+50)+'px');
+
+        })
+        .on('mouseleave',function(){
+            var tooltip = d3.select('.custom-tooltip')
+                .transition()
+                .style('opacity',0);
+        })
+}
+
+//function draw(dataSeries) {
+//
+//    plot.select('.data-line')
+//        .datum(dataSeries)
+//        .transition()
+//        .attr('d', lineGenerator);
+//    draw(tea);
